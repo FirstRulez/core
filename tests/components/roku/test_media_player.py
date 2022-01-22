6bb672a6620c025b54e6aa011687766b4625e96f
Chris Talkington <chris@talkingtontech.com>
2022-01-22 00:41:10 -0600

    Update test_media_player.py

--- tests/components/roku/test_media_player.py
+++ tests/components/roku/test_media_player.py
@@ -99,7 +99,7 @@ async def test_setup(hass: HomeAssistant, init_integration: MockConfigEntry) -> 
     assert entry.device_id
     device_entry = device_registry.async_get(entry.device_id)
     assert device_entry
-    assert device_entry.identifiers == {(DOMAIN, UPNP_SERIAL)}
+    assert device_entry.identifiers == {(DOMAIN, "1GU48T017973")}
     assert device_entry.connections == {
         (dr.CONNECTION_NETWORK_MAC, "b0:a7:37:96:4d:fb"),
         (dr.CONNECTION_NETWORK_MAC, "b0:a7:37:96:4d:fa"),
